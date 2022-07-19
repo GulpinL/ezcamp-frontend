ca5d3255966c5a1482a9591162f844cc7aecd76e
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-19 13:39:45 +0700

    update navbar + sidebar after login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,43 +1,46 @@
 import React, { useState } from 'react'
 import { BsSearch, BsCart3, BsCaretDown } from "react-icons/bs";
 import { FiUser } from "react-icons/fi";
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import "./Navbar.sass"
 
 const Navbar = () => {
-  const [accountName, setAccountName] = useState("Trần Hà Long")
+  const { user } = useSelector(store => store.user)
   const [cartAmount, setCartAmount] = useState(4)
-
   return (
-    <nav id = "navbar">
+    <nav id="navbar">
       <div className="navbar-container">
-        <Link to ="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand">
           <img src="/img/brand_logo.png" alt="" className="logo-brand" />
         </Link>
         <div className="navbar-search">
-          <input type="text" className="search-box" placeholder='Tent, gloves, stove, ...'/>
+          <input type="text" className="search-box" placeholder='Tent, gloves, stove, ...' />
           <button className='btn search-btn'>
-            <BsSearch className='search-icon'/>
+            <BsSearch className='search-icon' />
             Search
           </button>
         </div>
         <div className="navbar-account">
-          {accountName !== "" ? <div className='account'>
-            <FiUser className="account-icon"/>
+          {user.account !== "" ? <div className='account'>
+            <FiUser className="account-icon" />
             <div className="account-info">
               <p>Account</p>
               <div className="account-name">
-                <p className='name'>{accountName}</p>
-                <BsCaretDown className='account-down-icon'/>
+                <p className='name'>{user.account}</p>
+                <BsCaretDown className='account-down-icon' />
               </div>
             </div>
-          </div> : 
-          <button className='btn login-btn'>Login</button>}
+          </div> :
+            <Link to="/login">
+              <button className='btn login-btn'>Login</button>
+            </Link>
+          }
         </div>
         <div className="navbar-cart">
           <div className="cart">
-            <BsCart3 className='cart-icon'/>
+            <BsCart3 className='cart-icon' />
             <div className="cart-amount">{cartAmount}</div>
           </div>
         </div>
@@ -46,22 +49,22 @@ const Navbar = () => {
       <div className="navbar-container ">
         <ul className="navbar-list">
           <li className='navbar-item'>
-            <Link to = "/" className='navbar-link'>
+            <Link to="/" className='navbar-link'>
               Home
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to = "/" className='navbar-link'>
+            <Link to="/" className='navbar-link'>
               Product
             </Link>
           </li>
           <li className='navbar-item'>
-            <Link to ="/" className='navbar-link'>
+            <Link to="/" className='navbar-link'>
               About us
             </Link>
           </li>
           <li className='navbar-item'>
-            <Link to = "/" className='navbar-link'>
+            <Link to="/" className='navbar-link'>
               Contact
             </Link>
           </li>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
-import React, {useState} from 'react'
-import {useSelector, useDispatch} from "react-redux"
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux"
 import { Link } from 'react-router-dom'
 import { FaBars, FaUserAlt } from "react-icons/fa";
 import { BsCart3, BsSearch, BsHouseDoorFill, BsFillInfoCircleFill, BsFillTelephoneFill } from "react-icons/bs";
 import { FiLogIn, FiLogOut } from "react-icons/fi";
 import { GiLeatherBoot } from "react-icons/gi";
+
+import { pageList } from '../../common/pagesList/pageList';
 
 
 import { setIsSidebarOpen } from '../../features/Navbar/navbarSlice';
@@ -14,87 +16,74 @@ import "./Sidebar.sass"
 const Sidebar = () => {
 
   const dispatch = useDispatch()
-  const {isSidebarOpen} = useSelector(store => store.navbar)
+  const { isSidebarOpen } = useSelector(store => store.navbar)
+  const { user } = useSelector(store => store.user)
+
 
   const [cartAmount, setCartAmount] = useState(4)
-  const [accountName, setAccountName] = useState("Trần Hà Long")
+
   const handleSidebar = () => {
     dispatch(setIsSidebarOpen(!isSidebarOpen))
   }
   return (
     <aside id='sidebar'>
-        <div className="sidebar-container">
-          <Link to ="/" className="sidebar-brand">
-            <img src="/img/brand_logo.png" alt="" className="logo-brand" />
-          </Link>
-          <div className="sidebar-left">
-            <div className="navbar-cart">
-                <div className="cart">
-                  <BsCart3 className='cart-icon'/>
-                  <div className="cart-amount">{cartAmount}</div>
-                </div>
+      <div className="sidebar-container">
+        <Link to="/" className="sidebar-brand">
+          <img src="/img/brand_logo.png" alt="" className="logo-brand" />
+        </Link>
+        <div className="sidebar-left">
+          <div className="navbar-cart">
+            <div className="cart">
+              <BsCart3 className='cart-icon' />
+              <div className="cart-amount">{cartAmount}</div>
             </div>
-            <button className="sidebar-btn" onClick={handleSidebar}>
-              <FaBars className={isSidebarOpen ? 'sidebar-icon rotate-90' : 'sidebar-icon'}/>
-            </button>
           </div>
-        </div>
-        <div className="line"></div>
-        {isSidebarOpen ? <div className="sidebar-dropdown">
-          <div className="sidebar-search">
-            <input type="text" className="search-box sidebar-box" placeholder='Tent, gloves, stove, ...'/>
-            <button className='btn search-btn'>
-              <BsSearch className='search-icon'/>
-              Search
+          <button className="sidebar-btn" onClick={handleSidebar}>
+            <FaBars className={isSidebarOpen ? 'sidebar-icon rotate-90' : 'sidebar-icon'} />
           </button>
+        </div>
+      </div>
+      <div className="line"></div>
+      {isSidebarOpen ? <div className="sidebar-dropdown">
+        <div className="sidebar-search">
+          <input type="text" className="search-box sidebar-box" placeholder='Tent, gloves, stove, ...' />
+          <button className='btn search-btn'>
+            <BsSearch className='search-icon' />
+            Search
+          </button>
+        </div>
+        {user.account !== "" ? <Link to="/" className="sidebar-link">
+          <div className="sidebar-icon">
+            <FaUserAlt />
           </div>
-          {accountName !== "" ? <Link to = "/" className="sidebar-link">
+          <p>{user.account}</p>
+        </Link> : null}
+        {pageList.map((page, index) => {
+          const { path, name, icon } = page
+          return <Link key={index} to={path} className='sidebar-link' onClick={handleSidebar}>
             <div className="sidebar-icon">
-              <FaUserAlt/>
+              {icon}
             </div>
-            <p>{accountName}</p>
-          </Link> : null}
-          <Link to = "/" className='sidebar-link'>
-            <div className="sidebar-icon">
-              <BsHouseDoorFill/>
-            </div>
-            <p>Home</p>
+            <p>{name}</p>
           </Link>
-          <Link to = "/" className='sidebar-link'>
-            <div className="sidebar-icon">
-              <GiLeatherBoot/>
-            </div>
-            <p>Product</p>
-          </Link>
-          <Link to = "/" className='sidebar-link'>
-            <div className="sidebar-icon">
-              <BsFillInfoCircleFill/>
-            </div>
-            <p>About us</p>
-          </Link>
-          <Link to = "/" className='sidebar-link'>
-            <div className="sidebar-icon">
-              <BsFillTelephoneFill/>
-            </div>
-            <p>Contact</p>
-          </Link>
-          <div className="btns-container">
-            {accountName === "" ? <button className='btn sidebar-login-btn'>
+        })}
+        <div className="btns-container" onClick={handleSidebar}>
+          {user.account === "" ? <Link to="/login">
+            <button className='btn sidebar-login-btn'>
               <div className="sidebar-icon">
-                <FiLogIn/>
+                <FiLogIn />
               </div>
               <p>Login</p>
-            </button> : <button className='btn sidebar-login-btn'>
-              <div className="sidebar-icon">
-                <FiLogOut className='rotate-180'/>
-              </div>
-              <p>Logout</p>
-            </button>}
-            
-            
-          </div>
-        </div> :null}
-        
+            </button>
+          </Link> : <button className='btn sidebar-login-btn'>
+            <div className="sidebar-icon">
+              <FiLogOut className='rotate-180' />
+            </div>
+            <p>Logout</p>
+          </button>}
+        </div>
+      </div> : null}
+
     </aside>
   )
 }
