13e2bb92f1d26f3bb334f533133951028097645a
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-20 07:51:20 +0700

    create category

---
+++ src/components/Category/Catgory.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+
+import "./Category.sass"
+
+const Catgory = () => {
+    return (
+        <section className='category'>
+            <ul className="category-list">
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+            </ul>
+            <ul className="category-list">
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+                <li className="category-item">
+                    <div className="category-icon">
+                        <img src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn" alt="" />
+                    </div>
+                    <div className="category-name">
+                        Tent
+                    </div>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+export default Catgory
