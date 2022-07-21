9c107b52be52d12d7ce0bd3c1c8bd67330ee1386
tuandat290601 <63840138+tuandat290601@users.noreply.github.com>
2022-07-21 12:13:36 +0700

    update home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Advertisement, Category, ProductList } from '../../components'
 import {
   productList
 } from '../../common/productList/productList'
-
+import { BiCaretDown } from "react-icons/bi";
 import "./Home.sass"
+import { useViewport } from '../../customhooks';
 
 const Home = () => {
+  const viewport = useViewport()
+  const isMobileViewport = viewport.width < 1024
+
+  const [list, setList] = useState(productList.slice(0, 5))
+  const handleDisplayList = () => {
+    setList(productList.slice(0, list.length + 10))
+  }
   return (
-    <div id="home-page">
+    <div id="home-page" className={isMobileViewport ? 'pt-100' : ''}>
       <Advertisement />
       <Category />
-      <ProductList list={productList} />
+      <ProductList list={list} />
+      <div className="show-more-container">
+        <button className='btn show-more-btn' onClick={handleDisplayList}>Show more <BiCaretDown /> </button>
+      </div>
     </div>
   )
 }
